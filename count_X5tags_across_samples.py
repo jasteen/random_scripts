7eb5dec72725c5e7f9b8c54024291606b1efd816
Jason Steen <jason.steen@monash.edu.au>
2020-02-13 10:35:09 +1100

    sfg

--- count_X5tags_across_samples.py
+++ count_X5tags_across_samples.py
@@ -4,36 +4,55 @@ import pysam
 from collections import defaultdict
 
 
-files=["100000000_130313_cfDNA_2_capt_2_H5W35BCX2_TGTATGCG_connor_dedup.bam",
-"100000000_130313_cfDNA_capt_2_H5W35BCX2_TACAGGAT_connor_dedup.bam",
-"10000000_130313_cfDNA_2_capt_2_H5W35BCX2_TGTATGCG_connor_dedup.bam",
-"10000000_130313_cfDNA_capt_2_H5W35BCX2_TACAGGAT_connor_dedup.bam",
-"130313_cfDNA_2_capt_2_H5W35BCX2_TGTATGCG_connor_dedup.bam",
-"130313_cfDNA_capt_2_H5W35BCX2_TACAGGAT_connor_dedup.bam",
-"20000000_130313_cfDNA_2_capt_2_H5W35BCX2_TGTATGCG_connor_dedup.bam",
-"20000000_130313_cfDNA_capt_2_H5W35BCX2_TACAGGAT_connor_dedup.bam",
-"30000000_130313_cfDNA_2_capt_2_H5W35BCX2_TGTATGCG_connor_dedup.bam",
-"30000000_130313_cfDNA_capt_2_H5W35BCX2_TACAGGAT_connor_dedup.bam",
-"40000000_130313_cfDNA_2_capt_2_H5W35BCX2_TGTATGCG_connor_dedup.bam",
-"40000000_130313_cfDNA_capt_2_H5W35BCX2_TACAGGAT_connor_dedup.bam",
-"50000000_130313_cfDNA_2_capt_2_H5W35BCX2_TGTATGCG_connor_dedup.bam",
-"50000000_130313_cfDNA_capt_2_H5W35BCX2_TACAGGAT_connor_dedup.bam",
-"60000000_130313_cfDNA_2_capt_2_H5W35BCX2_TGTATGCG_connor_dedup.bam",
-"60000000_130313_cfDNA_capt_2_H5W35BCX2_TACAGGAT_connor_dedup.bam",
-"70000000_130313_cfDNA_2_capt_2_H5W35BCX2_TGTATGCG_connor_dedup.bam",
-"70000000_130313_cfDNA_capt_2_H5W35BCX2_TACAGGAT_connor_dedup.bam",
-"80000000_130313_cfDNA_2_capt_2_H5W35BCX2_TGTATGCG_connor_dedup.bam",
-"80000000_130313_cfDNA_capt_2_H5W35BCX2_TACAGGAT_connor_dedup.bam",
-"90000000_130313_cfDNA_2_capt_2_H5W35BCX2_TGTATGCG_connor_dedup.bam",
-"90000000_130313_cfDNA_capt_2_H5W35BCX2_TACAGGAT_connor_dedup.bam"]
+files=["/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130241-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130241-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130244-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130248-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130248-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130250-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130250-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130309-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130313-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130313-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130321-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130322-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130326-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130327-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130332-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130339-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130339-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130340-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130341-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130341-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130344-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130344-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130346-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130349-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130376-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130376-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130381-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130381-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130382-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130382-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130383-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130383-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130385-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130385-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130406-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130414-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130414-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130416-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130417-T.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130417-TP.bwa.sorted.connor.bam",
+"/scratch/uc23/hfettke/cfDNA_BAMs_41/post_connor/130463-T.bwa.sorted.connor.bam"]
 
 
 final={}
 file_short_list = []
 
 for file in files:
-    file_short = file.split("_S")
-    file_short_list.append(file_short[0])
+    file_short = file.split("/")
+    file_short_list.append(file_short[5])
     samfile = pysam.AlignmentFile(file, "rb")
     tags = []
 
@@ -51,11 +70,11 @@ for file in files:
 
     for x, y in counts.iteritems():
         if x in final:
-            final[x].update({file_short[0]: y/2})
+            final[x].update({file_short[5]: y/2})
         else:
-            final.update({x:{file_short[0]: y/2}})
+            final.update({x:{file_short[5]: y/2}})
 
-print final
+#print final
  
 
 with open('temp.txt', 'w') as outf:
